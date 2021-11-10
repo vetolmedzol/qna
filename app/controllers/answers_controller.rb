5a3473d12a43db719ef32ac0aa7dbdd29ca1f736
vetolmedzol <vetolmedzol@gmail.com>
2021-11-10 13:34:41 +0200

    added answers controller, tests and routes

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,35 @@
+class AnswersController < ApplicationController
+  before_action :find_question
+
+  def show
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def new
+    @answer = @question.answers.build
+  end
+
+  def create
+    @answer = @question.answers.build(answer_params)
+    if @answer.save
+      redirect_to(question_path(@question))
+    else
+      render(:new)
+    end
+  end
+
+  def destroy
+    @question.answers.find(params[:id]).destroy!
+    redirect_to(question_path(@question))
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:title)
+  end
+end
