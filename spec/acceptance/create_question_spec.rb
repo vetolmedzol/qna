b9a6e0951a24e5697d879fb58604ecb983223ecb
Vitalii Mozol <45063072+vetolmedzol@users.noreply.github.com>
2021-11-12 10:44:46 +0200

    added user, acceptance tests and some interface (#6)
    added user, acceptance tests and some interface

---
+++ spec/acceptance/create_question_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Create uqestions', '
+    In order to get answer fromm community
+    As an authenticated user
+    I want to be able toask question
+' do
+  given(:user) { create(:user) }
+
+  it 'Authenticated user creates questions' do
+    sign_in(user)
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'test question'
+    fill_in 'Body', with: 'test question body'
+    click_on 'Create'
+
+    expect(page).to(have_content('Your question successfully created!'))
+    expect(page).to(have_content('test question'))
+    expect(page).to(have_content('test question body'))
+  end
+
+  it 'Non-authenticated user tries to create questions' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to(have_content('You need to sign in or sign up before continuing.'))
+  end
+end
